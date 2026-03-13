b2a56837c86f4a32fc8035170f8eece4957c3c29
phongnt93 <92678176+phongnt93@users.noreply.github.com>
2026-03-13 14:58:38 +0700

    Add RequestMapping to HelloController

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
     @Value("${app.message:Hello from Spring Boot on K8s!}")
